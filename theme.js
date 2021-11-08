b335ce6d682a495ede8556921850dff30cb804b6
Julien Usson <julien@usson.me>
2021-11-08 23:03:52 +0100

    first commit

---
+++ theme.js
@@ -0,0 +1,17 @@
+import { themes } from 'mdx-deck'
+
+export default {
+    ...themes.syntaxHighlighterPrism,
+    fonts: {
+        body: 'Roboto, sans-serif',
+        monospace: '"Roboto Mono", monospace',
+    },
+    colors: {
+        text: '#23272F',
+        background: '#FFFFFF',
+        primary: '#087EA4',
+    },
+    text: {
+        heading: { color: "#087EA4" }
+    }
+}
